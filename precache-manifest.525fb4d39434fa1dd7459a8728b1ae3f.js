d95b27dd794619e280009b12567fa8f484595d18
dnorkett <dnorkett@gmail.com>
2020-04-09 18:15:32 -0500

    Updates

---
+++ precache-manifest.525fb4d39434fa1dd7459a8728b1ae3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac36a8f678698a520b5a9d7fbae7ae2",
+    "url": "/react-todoList-with-API/index.html"
+  },
+  {
+    "revision": "19c6814d4b8e965a2901",
+    "url": "/react-todoList-with-API/static/css/main.f3ed73bb.chunk.css"
+  },
+  {
+    "revision": "9bcd051f99195147da24",
+    "url": "/react-todoList-with-API/static/js/2.f3aa99f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todoList-with-API/static/js/2.f3aa99f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c6814d4b8e965a2901",
+    "url": "/react-todoList-with-API/static/js/main.008f6ffc.chunk.js"
+  },
+  {
+    "revision": "0dff75387183c4d8e7bc",
+    "url": "/react-todoList-with-API/static/js/runtime-main.4bcfc142.js"
+  }
+]);
